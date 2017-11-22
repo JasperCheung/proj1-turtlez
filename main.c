746e639b4d66a6c264005bba3ce248a9b971b708
Jeffrey Luo <luojeffrey@yahoo.com>
2017-11-22 11:42:27 -0500

    init

--- main.c
+++ main.c
@@ -3,4 +3,21 @@
 #include <unistd.h>
 
 int main() {
+  /*
+  char *args[3] = {"ls", "-l"};
+  args[2] = '\0';
+  
+  int f = fork();  
+  
+  if(f == 0) {
+    execvp(args[0], args);
+  }
+
+  if(f > 0)
+    printf("This is parent!\n");
+  */
+
+  printf("bash >");
+  
+  return 0;
 }
